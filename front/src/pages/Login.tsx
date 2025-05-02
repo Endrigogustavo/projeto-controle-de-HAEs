e42fb630190138e78d75d0554aa36ab6d2d5af7b
nickolss <nickolasmaiaraujo@gmail.com>
2025-05-01 22:32:19 -0300

    feat: added sign in and sign up screens

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,9 +1,77 @@
-function Login() {
-    return(
-        <>
-            <h2>Login</h2>
-        </>
-    )
-}
+import React from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { TextField, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton } from "@mui/material";
 
-export default Login
+export default function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="p-8 xl:w-xl">
+          <img
+            src="/fatec_zona_leste_icon.png"
+            alt="Logo da Fatec da Zona Leste"
+            className="mb-4 w-24"
+          />
+          <h1 className="text-xl my-4 font-semibold">
+            Sistema de Controle de
+            <span className="text-red-fatec"> HAEs</span>
+          </h1>
+
+          <form action="" className="flex flex-col">
+            <TextField
+              type="email"
+              label="E-mail institucional"
+              placeholder="nome@fatec.sp.gov.br"
+              sx={{ margin: "1rem 0" }}
+              required
+            />
+
+            <FormControl>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                required
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword
+                          ? "Esconder a senha"
+                          : "Mostrar a senha"
+                      }
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+
+            <a href="">Esqueceu a senha?</a>
+
+            <button
+              type="submit"
+              className="bg-red-fatec text-white p-2 rounded mt-6 mb-2 uppercase"
+            >
+              Entrar
+            </button>
+          </form>
+
+          <p>
+            É seu primeiro acesso? <a href="">Cadastre-se</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -1,9 +1,82 @@
-function Register(){
-    return(
-        <>
-            <h2>Register</h2>
-        </>
-    )
-}
+import React from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 
-export default Register
+export default function Register() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="p-8 xl:w-xl">
+          <img
+            src="/fatec_zona_leste_icon.png"
+            alt="Logo da Fatec da Zona Leste"
+            className="mb-4 w-24"
+          />
+          <h1 className="text-xl my-4 font-semibold">
+            Sistema de Controle de
+            <span className="text-red-fatec"> HAEs</span>
+          </h1>
+
+          <form action="" className="flex flex-col">
+            <TextField required label="Nome" />
+
+            <TextField
+              type="email"
+              label="E-mail institucional"
+              placeholder="nome@fatec.sp.gov.br"
+              sx={{ margin: "1rem 0" }}
+              required
+            />
+
+            <FormControl>
+              <InputLabel htmlFor="password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="password"
+                type={showPassword ? "text" : "password"}
+                required
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword ? "Esconder a senha" : "Mostrar a senha"
+                      }
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+
+            <button
+              type="submit"
+              className="bg-red-fatec text-white p-2 rounded my-2 uppercase"
+            >
+              Cadastrar
+            </button>
+          </form>
+
+          <p>
+            Já possui uma conta? <a href="/login">Entre</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
