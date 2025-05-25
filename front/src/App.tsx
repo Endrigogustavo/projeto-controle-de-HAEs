e1def18bd33eacb8cfd055f244787a393b3ed430
nickolss <nickolasmaiaraujo@gmail.com>
2025-05-25 16:28:12 -0300

    fix: fixed routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from "@pages/Login"
-import Register from '@pages/Register'
-import VerificationCode from '@pages/VerificationCode'
-import RequestHae from '@pages/RequestHae'
+import Login from "@/pages/Login"
+import Register from '@/pages/Register'
+import VerificationCode from '@/pages/VerificationCode'
+import RequestHae from '@/pages/RequestHae'
 
 function App() {
   return (
