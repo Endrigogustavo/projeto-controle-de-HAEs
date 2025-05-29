a4161e61ff6ed7cb67d84f06c403a58ca800c342
nickolss <nickolasmaiaraujo@gmail.com>
2025-05-28 21:08:14 -0300

    feat: added new routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,51 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from "@/pages/Login"
-import Register from '@/pages/Register'
-import VerificationCode from '@/pages/VerificationCode'
-import RequestHae from '@/pages/RequestHae'
+// src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "@pages/Login";
+import Register from "@pages/Register";
+import VerificationCode from "@pages/VerificationCode";
+import RequestHae from "@pages/RequestHae";
+import Dashboard from "@pages/Dashboard";
+import MyRequests from "@pages/MyRequests";
+import { PrivateRoute } from "@components/PrivateRoute";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={ <Register /> }/>
-        <Route path='/verificationCode' element={ <VerificationCode /> }/>
-        <Route path='/requestHae' element={ <RequestHae /> } />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* Rotas públicas */}
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/verificationCode" element={<VerificationCode />} />
+				
+				{/* Rotas privadas */}
+				<Route
+					path="/"
+					element={
+						<PrivateRoute>
+							<Dashboard />
+						</PrivateRoute>
+					}
+				/>
+
+				<Route
+					path="/requestHae"
+					element={
+						<PrivateRoute>
+							<RequestHae />
+						</PrivateRoute>
+					}
+				/>
+				<Route
+					path="/myrequests"
+					element={
+						<PrivateRoute>
+							<MyRequests />
+						</PrivateRoute>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ front/src/components/PrivateRoute.tsx
@@ -0,0 +1,28 @@
+import { JSX, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import api from "@services/index";
+
+interface PrivateRouteProps {
+	children: JSX.Element;
+}
+
+export function PrivateRoute({ children }: PrivateRouteProps) {
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+	useEffect(() => {
+		api
+			.get("/ROTA PARA VERIFICAR TOKEN", { withCredentials: true })
+			.then(() => setIsAuthenticated(true))
+			.catch(() => setIsAuthenticated(false));
+	}, []);
+
+	if (isAuthenticated === null) {
+		return <div>Loading...</div>;
+	}
+
+	if (!isAuthenticated) {
+		return <Navigate to="/login" replace />;
+	}
+
+	return children;
+}
