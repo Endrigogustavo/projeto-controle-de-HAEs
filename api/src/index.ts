2c06d3276b449e191eec9a0f8c4ab6e13699a204
nickolss <nickolasmaiaraujo@gmail.com>
2025-05-30 20:25:12 -0300

    fix: fixed added dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,7 @@
 import { server } from "./server";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 server.listen(process.env.PORT, () =>
   console.log(`Server is running! on Port: ${process.env.PORT}!`)
--- api/src/server.ts
+++ api/src/server.ts
@@ -3,17 +3,17 @@ import "./shared/utils/yupTranslation.utils";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-
+import dotenv from 'dotenv';
+dotenv.config();
 import { routes } from "./routes/index";
 
 const server = express();
 
 server.use(
 	cors({
-		origin: (origin, callback) => {
-			callback(null, origin || true);
-		},
+		origin: ["http://localhost:5173", "http://localhost", "http://front", "http://front:5173"],
 		credentials: true,
+		methods: "GET,POST,PUT,DELETE",
 	})
 );
 
