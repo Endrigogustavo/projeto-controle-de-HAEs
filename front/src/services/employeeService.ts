e85f3690c439727709105cf053120cd44de9c0d6
nickolss <nickolasmaiaraujo@gmail.com>
2025-07-22 22:22:45 -0300

    fix: fixed get my user route

--- front/src/services/employeeService.ts
+++ front/src/services/employeeService.ts
@@ -1,11 +1,11 @@
-import { getProfessor } from "./employee";
+import { getMyUser } from "./employee";
 interface IAuthService {
 	getProfessor: (id: string) => Promise<any>;
 }
 
 const employeeService: IAuthService = {
-	getProfessor: async (id) => {
-		return await getProfessor(id);
+	getProfessor: async () => {
+		return await getMyUser();
 	},
 };
 
