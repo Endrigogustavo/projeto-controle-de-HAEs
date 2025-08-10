903d94e70a758969f8caf9b021f3113bf166a339
Endrigogustavo <endrigogustavo2006@gmail.com>
2025-08-10 16:11:33 -0300

    fix: key cloudinary informations were implemented on .env.example and cloudinary config

--- api/src/main/java/br/com/fateczl/apihae/driver/config/cloudinary/CloudinaryConfig.java
+++ api/src/main/java/br/com/fateczl/apihae/driver/config/cloudinary/CloudinaryConfig.java
@@ -1,19 +1,30 @@
 package br.com.fateczl.apihae.driver.config.cloudinary;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${CLOUDINARY_NAME}")
+    private String cloudinaryName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String cloudinaryApiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String cloudinaryApiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "*********",
-            "api_key", "*********",
-            "api_secret", "*********",
+            "cloud_name", cloudinaryName,
+            "api_key", cloudinaryApiKey,
+            "api_secret", cloudinaryApiSecret,
             "secure", true));
     }
 }
