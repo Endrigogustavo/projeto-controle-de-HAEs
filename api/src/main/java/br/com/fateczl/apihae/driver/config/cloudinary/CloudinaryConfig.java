86ae4e6a707bf61a2fc0fddf013beae7944b240e
gustavoventieri <ventierigustavo@gmail.com>
2025-08-11 12:32:44 -0300

    feat: add cloudinary config variables to application.yml

--- api/src/main/java/br/com/fateczl/apihae/driver/config/cloudinary/CloudinaryConfig.java
+++ api/src/main/java/br/com/fateczl/apihae/driver/config/cloudinary/CloudinaryConfig.java
@@ -1,30 +1,32 @@
 package br.com.fateczl.apihae.driver.config.cloudinary;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
-    @Value("${CLOUDINARY_NAME}")
-    private String cloudinaryName;
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
 
-    @Value("${CLOUDINARY_API_KEY}")
-    private String cloudinaryApiKey;
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
 
-    @Value("${CLOUDINARY_API_SECRET}")
-    private String cloudinaryApiSecret;
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", cloudinaryName,
-            "api_key", cloudinaryApiKey,
-            "api_secret", cloudinaryApiSecret,
-            "secure", true));
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        return new Cloudinary(config);
     }
 }
