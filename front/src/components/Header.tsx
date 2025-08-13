680aa570aa061d0d919f9307143a56d7e208a7b7
gustavoventieri <ventierigustavo@gmail.com>
2025-08-13 00:44:57 -0300

    feat: title color changed to red

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
 
   return (
     <header className="col-start-2 row-start-1 border-b border-gray-400 shadow-black/10 bg-white flex items-center justify-between p-4 ">
-      <h1 className="text-2xl font-bold text-gray-800">
+      <h1 className="text-2xl font-bold text-red-600">
         Sistema de Controle de HAEs
       </h1>
       <EmployeeInfoDisplay employee={employee} isLoading={isLoadingEmployee} />
