e70284b0556db07f9efbc03d3948cedf84e2d4c3
Endrigogustavo <endrigogustavo2006@gmail.com>
2025-08-19 12:01:29 -0300

    feat: new hae comprovante schema were created to implement

---
+++ api/src/main/java/br/com/fateczl/apihae/domain/entity/ComprovationHae.java
@@ -0,0 +1,10 @@
+package br.com.fateczl.apihae.domain.entity;
+
+import java.util.List;
+
+public class ComprovationHae {
+    private String id;
+    private String title;
+    private String employeeId;
+    private List<String> comprovationsURLs;
+}
